a9c4522beade11d59dae11e0eef2d548c95f68b4
Benson Imoh, ST <benson@stbensonimoh.com>
2019-07-11 16:50:17 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 set -e
-sudo apt-get install -y lftp
+sudo apt-get install -y lftp;
 lftp -c "set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate false; open ftp://$SFTP_USER:$SFTP_PASSWORD@$SFTP_HOST:21; mirror -eRv -x ^\.git/ . /var/www/html/testing; quit;"
 echo "successful"
  
